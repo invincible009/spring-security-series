308c3eb308128d0fd765cd1fef174ac1a0c88de8
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-07-18 23:40:04 +0100

    added authority entity

---
+++ src/main/java/com/shimadove/springsecurityseries/entity/Authorities.java
@@ -0,0 +1,55 @@
+package com.shimadove.springsecurityseries.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Authorities {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String username;
+    private String authority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String toString() {
+        return "Authorities{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
+}
