b44b6ae3f10f70fa3f40e8ed466b250bc5f45efa
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-07-18 23:40:28 +0100

    update controller base url

--- src/main/java/com/shimadove/springsecurityseries/api/BalanceController.java
+++ src/main/java/com/shimadove/springsecurityseries/api/BalanceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sdl")
+@RequestMapping("/sdl/balance")
 public class BalanceController {
 	
 	@GetMapping("/myBalance")
--- src/main/java/com/shimadove/springsecurityseries/api/ContactController.java
+++ src/main/java/com/shimadove/springsecurityseries/api/ContactController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/sdl")
+@RequestMapping("/sdl/contact")
 public class ContactController {
 	
 	@GetMapping("/contact")
