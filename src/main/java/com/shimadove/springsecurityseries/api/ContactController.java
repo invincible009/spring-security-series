a0db59ba449ccbb513ed47ff5e372cad1b6f0d6b
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-07-18 15:36:08 +0100

    add contact controller

--- src/main/java/com/shimadove/springsecurityseries/api/ContactController.java
+++ src/main/java/com/shimadove/springsecurityseries/api/ContactController.java
@@ -1,9 +1,11 @@
 package com.shimadove.springsecurityseries.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/sdl")
 public class ContactController {
 	
 	@GetMapping("/contact")
