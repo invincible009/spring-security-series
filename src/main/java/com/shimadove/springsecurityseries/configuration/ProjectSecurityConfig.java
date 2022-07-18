1a200abdcb92d261ce4577f1de97644772ed9346
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-07-18 15:38:24 +0100

    add spring security configuration class
    handle all spring configuration and fine access control from a single location.

--- src/main/java/com/shimadove/springsecurityseries/configuration/ProjectSecurityConfig.java
+++ src/main/java/com/shimadove/springsecurityseries/configuration/ProjectSecurityConfig.java
@@ -7,48 +7,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	/**
-	 * /myAccount - Secured /myBalance - Secured /myLoans - Secured /myCards -
-	 * Secured /notices - Not Secured /contact - Not Secured
-	 */
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		/**
-		 * Default configurations which will secure all the requests
-		 */
-
-		/*
-		 * http .authorizeRequests() .anyRequest().authenticated() .and()
-		 * .formLogin().and() .httpBasic();
-		 */
-
-		/**
-		 * Custom configurations as per our requirement
-		 */
-
-		/*
-		 * http .authorizeRequests() .antMatchers("/myAccount").authenticated()
-		 * .antMatchers("/myBalance").authenticated()
-		 * .antMatchers("/myLoans").authenticated()
-		 * .antMatchers("/myCards").authenticated() .antMatchers("/notices").permitAll()
-		 * .antMatchers("/contact").permitAll() .and() .formLogin().and() .httpBasic();
-		 */
-
-		/**
-		 * Configuration to deny all the requests
-		 */
-
-		/*
-		 * http .authorizeRequests() .anyRequest().denyAll() .and() .formLogin().and()
-		 * .httpBasic();
-		 */
-
-		/**
-		 * Configuration to permit all the requests
-		 */
-
-		http .authorizeRequests() .anyRequest().permitAll().and() .formLogin().and()
+		http
+				.authorizeRequests()
+				.anyRequest()
+				.permitAll()
+				.and()
+				.formLogin()
+				 .and()
 		 .httpBasic();
 		
 
