f859e031043efa9327fab1d147fa00740c51f218
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-07-19 16:32:37 +0100

    add spring authentication to endpoints

--- src/main/java/com/shimadove/springsecurityseries/configuration/ProjectSecurityConfig.java
+++ src/main/java/com/shimadove/springsecurityseries/configuration/ProjectSecurityConfig.java
@@ -1,8 +1,21 @@
 package com.shimadove.springsecurityseries.configuration;
 
+import ch.qos.logback.core.joran.action.NOPAction;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -13,8 +26,13 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http
 				.authorizeRequests()
-				.anyRequest()
-				.permitAll()
+				.antMatchers("/account").authenticated()
+				.antMatchers("/cards").authenticated()
+				.antMatchers("/loans").authenticated()
+				.antMatchers("/balance").authenticated()
+				.antMatchers("/notices").permitAll()
+				.antMatchers("/contact").permitAll()
+				.antMatchers("/home").permitAll()
 				.and()
 				.formLogin()
 				 .and()
@@ -23,4 +41,15 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	}
 
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource){
+		return new JdbcUserDetailsManager(dataSource);
+	}
+
+
+	//With these the application will successfully authenticate the user
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
