d2dfd64d34e466d02be2c68de3da97c122735cb6
Gabriel Simon Mbaiorga <gabrielmbaiorga@gmail.com>
2022-07-21 14:36:37 +0100

    added userdetail service class

--- src/main/java/com/shimadove/springsecurityseries/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.shimadove.springsecurityseries.service;
-
-import com.shimadove.springsecurityseries.dtos.UserDTO;
-import com.shimadove.springsecurityseries.dtos.UserRegistrationResponseDTO;
-import com.shimadove.springsecurityseries.exception.UserAlreadyExistException;
-
-public interface IUserService {
-    UserRegistrationResponseDTO registerNewUserAccount(UserDTO userDto) throws UserAlreadyExistException;
-}
--- src/main/java/com/shimadove/springsecurityseries/service/tets.java
+++
@@ -1,4 +0,0 @@
-package com.shimadove.springsecurityseries.service;
-
-public class tets {
-}
